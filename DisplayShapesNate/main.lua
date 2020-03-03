28abcb7a5e1570c4a9083fd0e02f2260dc66f485
Nate MacAvoy <nathaniel.macavoy@ocsbstudent.ca>
2020-03-03 13:55:22 -0500

    Update main.lua

--- DisplayShapesNate/main.lua
+++ DisplayShapesNate/main.lua
@@ -5,3 +5,14 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set colour of the background 
+display.setDefault("background", 255/255, 255/255, 255/255)
+
+local myRectangle = display.newRect( 250, 200, 300, 150 )
+myRectangle.strokeWidth = 3
+myRectangle:setFillColor( 255/255, 0/255, 0/255 )
+myRectangle:setStrokeColor( 0/255, 0/255, 0/255 )
