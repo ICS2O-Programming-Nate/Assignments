45af5fa1210168da9d47dc62911d70399e04e3ae
Nate MacAvoy <nathaniel.macavoy@ocsbstudent.ca>
2020-03-04 14:30:29 -0500

    1

--- AnimatingImagesNate/main.lua
+++ AnimatingImagesNate/main.lua
@@ -5,3 +5,12 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set background 
+local backgroundImage = display.newImageRect("Images/background.jpg", 2048, 1536)
+local Catbug = display.newImage("Images/Catbug.png", 200, 200)
+local Oswald = display.newImage("Images/Oswald.png", 500, 500)
+local Bear = display.newImage("Images/Bear.png", 800, 200)
