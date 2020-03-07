3d754c55f5c2590569bd3b4583ba1a9cfdd381e3
Nate MacAvoy <nathaniel.macavoy@ocsbstudent.ca>
2020-03-06 19:24:12 -0500

    4

--- DisplayShapesNate/main.lua
+++ DisplayShapesNate/main.lua
@@ -13,6 +13,34 @@ display.setStatusBar(display.HiddenStatusBar)
 display.setDefault("background", 255/255, 255/255, 255/255)
 
 local myRectangle = display.newRect( 250, 200, 300, 150 )
-myRectangle.strokeWidth = 3
+myRectangle.strokeWidth = 1
 myRectangle:setFillColor( 255/255, 0/255, 0/255 )
 myRectangle:setStrokeColor( 0/255, 0/255, 0/255 )
+
+local myCircle = display.newCircle( 100, 100, 100 )
+myCircle:setFillColor( 0/225, 0/255, 255/255 )
+myCircle.x = 800
+myCircle.y = 200
+
+local halfW = display.contentWidth * 0.5
+local halfH = display.contentHeight * 0.5
+
+local vertices = { 0,-110, 105,-35, 65,90, -65,90, -105,-35, }
+
+local pentagon = display.newPolygon( halfW, halfH, vertices )
+pentagon.strokeWidth = 0
+pentagon:setStrokeColor( 1, 0, 0 )
+pentagon.x = 200
+pentagon.y = 500
+pentagon:setFillColor( 0, 0, 0 )
+
+local halfW = display.contentWidth * 0.5
+local halfH = display.contentHeight * 0.5
+
+local vertices = { 0,-110, 105,-35, 65,90, -65,90, }
+local octagon = display.newPolygon( halfW, halfH, vertices )
+octagon.strokeWidth = 5
+octagon:setStrokeColor( 0, 0, 0 )
+octagon.x = 800
+octagon.y = 500
+octagon:setFillColor( 1, 1, 1 )
