151504ebacd88223a9ea7058a8682d242ef7a1f0
Nate MacAvoy <nathaniel.macavoy@ocsbstudent.ca>
2020-03-10 13:58:28 -0400

    2

--- AnimatingImagesNate/main.lua
+++ AnimatingImagesNate/main.lua
@@ -14,3 +14,10 @@ local backgroundImage = display.newImageRect("Images/background.jpg", 2048, 1536
 local Catbug = display.newImage("Images/Catbug.png", 200, 200)
 local Oswald = display.newImage("Images/Oswald.png", 500, 500)
 local Bear = display.newImage("Images/Bear.png", 800, 200)
+
+Catbug.x = 100
+
+local function MoveCatbug(event)
+	Catbug.x = Catbug.x + scrollSpeed 
+end 
+Runtime:addEventListener("enterFrame", MoveCatbug)
