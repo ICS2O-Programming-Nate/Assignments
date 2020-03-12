5bb9e5fa98fab3b39b25879a1ab9689fa523d93e
Nate MacAvoy <nathaniel.macavoy@ocsbstudent.ca>
2020-03-11 21:54:31 -0400

    finished

--- AnimatingImagesNate/main.lua
+++ AnimatingImagesNate/main.lua
@@ -1,50 +1,80 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+------------------------------------------------------
+-- Title: Animating Images
+-- Name: Nate
+-- Course: ICS2O/3C
+-- This program moves images in differet directions with text
+------------------------------------------------------
 
--- Your code here
 
 -- hide status bar
 display.setStatusBar(display.HiddenStatusBar)
 
--- set background 
+-- set local variables  
 local backgroundImage = display.newImageRect("Images/background.jpg", 2048, 1536)
-local Catbug = display.newImage("Images/Catbug.png", 200, 200)
+local WaddleDee = display.newImage("Images/WaddleDee.png", 200, 200)
 local Oswald = display.newImage("Images/Oswald.png", 400, 500)
 local Bear = display.newImage("Images/Bear.png", 800, 200)
 
+--------------------------------------------------------
+-- scroll speeds 
 scrollSpeed1 = 1
 scrollSpeed2 = 2
 scrollSpeed3 = 3
 
-Catbug.alpha = 0
-Catbug.x = 200
-Catbug.y = 200
-local function MoveCatbug(event)
-	Catbug.x = Catbug.x + scrollSpeed3
-	Catbug.alpha = Catbug.alpha + 0.01 
+--------------------------------------------------------
+-- WaddleDee function
+--------------------------------------------------------
+
+WaddleDee.alpha = 0
+WaddleDee.x = 200
+WaddleDee.y = 350
+local function MoveWaddleDee(event)
+	WaddleDee.x = WaddleDee.x + scrollSpeed3
+	WaddleDee.y = WaddleDee.y - scrollSpeed1
+	WaddleDee.alpha = WaddleDee.alpha + 0.01
+	--make image rotate
+	WaddleDee:rotate(-0.09)
+ 
 end 
 	
-Runtime:addEventListener("enterFrame", MoveCatbug)
+Runtime:addEventListener("enterFrame", MoveWaddleDee)
 
+--------------------------------------------------------
+-- Bear function
+--------------------------------------------------------
 
 
 local function MoveBear(event)
 	Bear.x = Bear.x - scrollSpeed2
 	Bear.alpha = Bear.alpha - 0.001 
+	--make image rotate
+	Bear:rotate(0.1)
 end 
 
 Runtime:addEventListener("enterFrame", MoveBear)
 
+--------------------------------------------------------
+-- Oswald function
+--------------------------------------------------------
+
+-- clarity of image 
 Oswald.alpha = 0.1
 local function MoveOswald(event)
 	Oswald.x = Oswald.x + scrollSpeed3
 	Oswald.y = Oswald.y + scrollSpeed1
 	Oswald.alpha = Oswald.alpha + 0.009
+	--make image rotate
+	Oswald:rotate(5)
 end 
 	
 Runtime:addEventListener("enterFrame", MoveOswald)
 
+--------------------------------------------------------
+-- text operations
+--------------------------------------------------------
+
+-- set text 
+textObject = display.newText( "Greetings", 500, 50, nil, 50 )
+-- sets the colour of the text 
+textObject:setTextColor(225/225, 50/225, 170/225)
 
